2ea4f669da47861719f240f783dcd4e4a7979f6f
Trishane <trishaneww@gmail.com>
2024-07-22 10:48:28 -0400

    creates user to database

--- components/forms/RegisterForm.tsx
+++ components/forms/RegisterForm.tsx
@@ -12,7 +12,9 @@ import { UserFormValidation } from "@/lib/validation"
 import { useRouter } from "next/navigation"
 import { createUser } from "@/lib/actions/patient.actions"
 import { FormFieldType } from "./PatientForm"
-import { RadioGroup } from "../ui/radio-group"
+import { RadioGroup, RadioGroupItem } from "../ui/radio-group"
+import { GenderOptions } from "@/constants"
+import { Label } from "../ui/label"
 
  
 const RegisterForm = ({ user }: {user: User}) => {
@@ -116,8 +118,14 @@ const RegisterForm = ({ user }: {user: User}) => {
                     <RadioGroup className="flex h-11 gap-6 xl:justify-between"
                     onValueChange={field.onChange}
                     defaultValue={field.value}>
-                        {}
-                        
+                        {GenderOptions.map((option) => (
+                            <div key={option} className="radio-group">
+                                <RadioGroupItem value={option} id={option} />
+                                <Label htmlFor={option} className="cursor-point">
+                                    {option}
+                                </Label>
+                            </div>
+                        ))}
                     </RadioGroup>
                 </FormControl>
             )}
