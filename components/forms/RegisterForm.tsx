75a0265a6f4a60f5b51dc15cbe84628d36587b5e
Trishane <trishaneww@gmail.com>
2024-07-22 00:23:07 -0400

    created register form

---
+++ components/forms/RegisterForm.tsx
@@ -0,0 +1,84 @@
+"use client"
+ 
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { Button } from "@/components/ui/button"
+import { Form } from "@/components/ui/form"
+import CustomFormField from "../CustomFormField"
+import SubmitButton from "../ui/SubmitButton"
+import { useState } from "react"
+import { UserFormValidation } from "@/lib/validation"
+import { useRouter } from "next/navigation"
+import { createUser } from "@/lib/actions/patient.actions"
+import { FormFieldType } from "./PatientForm"
+
+ 
+const RegisterForm = ({ user }: {user: User}) => {
+  const router = useRouter()
+  const [isLoading, setIsLoading] = useState(false);
+  const form = useForm<z.infer<typeof UserFormValidation>>({
+    resolver: zodResolver(UserFormValidation),
+    defaultValues: {
+      name: "",
+      email: "",
+      phone: "",
+    },
+  })
+ 
+  const onSubmit = async (values: z.infer<typeof UserFormValidation>) => {
+    setIsLoading(true);
+
+    try {
+      const user = {
+        name: values.name,
+        email: values.email,
+        phone: values.phone,
+      };
+
+      console.log(user)
+
+      const newUser = await createUser(user);
+      console.log(newUser)
+
+      if (newUser) {
+        router.push(`/patients/${newUser.$id}/register`);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+
+    setIsLoading(false);
+  };
+
+  return (
+    <Form {...form}>
+    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-12 flex-1">
+      <section className="space-y-4">
+        <h1 className="header">Welcome</h1>
+        <p className="text-dark-700">Let us know more about yourself</p>
+      </section>
+
+      <section className="space-y-6">
+        <h2 className="sub-header">Personal Information</h2>
+      </section>
+
+      {/* ensurs no type errors */}
+      <CustomFormField 
+        fieldType={FormFieldType.INPUT}
+        control={form.control}
+        name="name"
+        label="Full Name"
+        placeholder="John Doe"
+        iconSrc="/assets/icons/user.svg"
+        iconAlt="user"
+      />
+
+
+      <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
+    </form>
+    </Form>
+  )
+}
+
+export default RegisterForm
