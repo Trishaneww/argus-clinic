100a857b814bd3b58a011b3e52076e2634f63293
Trishane <trishaneww@gmail.com>
2024-07-22 11:40:25 -0400

    emergency phone field

--- components/forms/RegisterForm.tsx
+++ components/forms/RegisterForm.tsx
@@ -150,15 +150,7 @@ const RegisterForm = ({ user }: {user: User}) => {
           />
       </div>
 
-      <section className="space-y-6">
-        <div className="mb-9 space-y-1">
-            <h2 className="sub-header">Medical Information</h2>
-        </div>
-      </section>
-
-
-
-
+  
 
       <div className="flex flex-col gap-6 xl:flex-row">
         <CustomFormField 
@@ -172,11 +164,25 @@ const RegisterForm = ({ user }: {user: User}) => {
         <CustomFormField 
             fieldType={FormFieldType.PHONE_INPUT}
             control={form.control}
-            name="phone"
-            label="phone"
+            name="emergencyContactNumber"
+            label="Emergency contact number"
             placeholder="(905)-672-2328"
         />
       </div>
+
+      <section className="space-y-6">
+        <div className="mb-9 space-y-1">
+            <h2 className="sub-header">Medical Information</h2>
+        </div>
+      </section>
+
+      <CustomFormField 
+            fieldType={FormFieldType.PHONE_INPUT}
+            control={form.control}
+            name="emergencyContactNumber"
+            label="Emergency contact number"
+            placeholder="(905)-672-2328"
+        />
 
       <div className="flex flex-col gap-6 xl:flex-row">
       
