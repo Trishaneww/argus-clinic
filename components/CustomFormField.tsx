14603982f2fe312ae7bc840d5422251f2d5092c8
Trishane <trishaneww@gmail.com>
2024-07-22 12:52:00 -0400

    checkbox consent component

--- components/CustomFormField.tsx
+++ components/CustomFormField.tsx
@@ -21,6 +21,7 @@ import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
 import { Select, SelectContent, SelectTrigger, SelectValue } from "./ui/select"
 import { Textarea } from "./ui/textarea"
+import { Checkbox } from "./ui/checkbox"
 
 interface CustomProps {
     control: Control<any>,
@@ -127,6 +128,21 @@ const RenderField = ({ field, props}: { field: any; props: CustomProps }) => {
                     />
                 </FormControl>
             )
+            case FormFieldType.CHECKBOX:
+                return (
+                  <FormControl>
+                    <div className="flex items-center gap-4">
+                      <Checkbox
+                        id={props.name}
+                        checked={field.value}
+                        onCheckedChange={field.onChange}
+                      />
+                      <label htmlFor={props.name} className="checkbox-label">
+                        {props.label}
+                      </label>
+                    </div>
+                  </FormControl>
+                );
         default:
             break;
    }
