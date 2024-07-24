bc6741affe2373639253dff2f183017fc665deac
Trishane <trishaneww@gmail.com>
2024-07-24 15:58:09 -0400

    created reusbale stacard component

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,3 +1,4 @@
+import StatCard from '@/components/StatCard'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
@@ -22,6 +23,29 @@ const Admin = () => {
             <section className="w-full  space-y-4">
                 <h1 className="header">Welcome</h1>
                 <p className="text-dark-700">Start the day with managing new appointments</p>
+            </section>
+
+            <section className="admin-stat">
+                <StatCard
+                    type="appointments"
+                    count={5}
+                    label="Scheduled Appointments"
+                    icon="/assets/icons/appointments.svg" 
+                />
+
+                <StatCard
+                    type="pending"
+                    count={5}
+                    label="Pending Appointments"
+                    icon="/assets/icons/pending.svg" 
+                />
+
+                <StatCard
+                    type="cancelled"
+                    count={5}
+                    label="Cancelled Appointments"
+                    icon="/assets/icons/cancelled.svg" 
+                />
             </section>
         </main>
     </div>
