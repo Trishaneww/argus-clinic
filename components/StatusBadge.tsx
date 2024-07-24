5072aa83cdf5cca13ebbf03c7ff6d1e16d244c5d
Trishane <trishaneww@gmail.com>
2024-07-24 17:30:12 -0400

    created update modal

--- components/StatusBadge.tsx
+++ components/StatusBadge.tsx
@@ -1,8 +1,30 @@
+import { StatusIcon } from '@/constants'
+import clsx from 'clsx'
+import Image from 'next/image'
 import React from 'react'
 
-const StatusBadge = () => {
+const StatusBadge = ({ status }: { status: Status}) => {
   return (
-    <div>StatusBadge</div>
+    <div className={clsx('status-badge', {
+        'bg-green-500': status === "scheduled",
+        'bg-blue-600': status === 'pending',
+        'bg-red-600': status === 'cancelled'
+    })}>
+        {/* status icon changes based on the value of status */}
+        <Image 
+            src={StatusIcon[status]}
+            alt={status}
+            width={24}
+            height={24}
+            className="h-fit w-3"
+        />
+        <p className={clsx('text-12-semibold capitilize', {
+            'text-green-500': status === 'scheduled',
+            'text-blue-500': status === 'pending',
+            'text-red-500': status === 'cancelled'
+        })}>{status}</p>
+
+    </div>
   )
 }
 
