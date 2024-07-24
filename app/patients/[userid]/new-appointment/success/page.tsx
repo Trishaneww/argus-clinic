1314dbd392065735f9d93815853c941344ea63a4
Trishane <trishaneww@gmail.com>
2024-07-24 03:02:14 -0400

    retrieve doctor information through params

--- app/patients/[userid]/new-appointment/success/page.tsx
+++ app/patients/[userid]/new-appointment/success/page.tsx
@@ -1,8 +1,15 @@
+import { Doctors } from '@/constants';
+import { getAppointment } from '@/lib/actions/appontment.actions';
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
-const Success = () => {
+const Success = async ({ params: { userId}, searchParams }: SearchParamProps) => {
+    const appointmentId = (searchParams?.appointmentId as string) || "";
+    const appointment = await getAppointment(appointmentId);
+
+    const doctor = Doctors.find((doc) => doc.name === appointment.primaryPhysician)
+
   return (
     <div className="flex h-screen max-h-screen px-[5%]">
         <div className="success-img">
@@ -34,11 +41,16 @@ const Success = () => {
             <section className="request-details">
                 <p>Requested appointment details:</p>
                 <div className="flex items-center gap-3">
-                    {/* <Image 
-
-
-                    /> */}
+                    <Image 
+                        src={doctor?.image!}
+                        alt="doctor"
+                        width={100}
+                        height={100}
+                        className="size-6"
+                    />
+                    <p className="whitespace-nowrap">Dr. {doctor?.name}</p>           
                 </div>
+                
             </section>
         </div>
     </div>
