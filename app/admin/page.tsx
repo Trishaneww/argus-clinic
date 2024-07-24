893685c79bcd0caefc2f50fb42dfc8fcaa689bf4
Trishane <trishaneww@gmail.com>
2024-07-24 16:46:42 -0400

    imported datatable

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,9 +1,13 @@
+import DataTable from '@/components/table/DataTable'
 import StatCard from '@/components/StatCard'
+import { getRecentAppointmentList } from '@/lib/actions/appontment.actions'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import columns from '@/components/table/columns'
 
-const Admin = () => {
+const Admin = async () => {
+    const appointments = await getRecentAppointmentList()
   return (
     <div className="mx-auto flex max-w-7xl flex-col space-y-14">
         <header className="admin-header">
@@ -28,25 +32,27 @@ const Admin = () => {
             <section className="admin-stat">
                 <StatCard
                     type="appointments"
-                    count={5}
+                    count={appointments.scheduledCount}
                     label="Scheduled Appointments"
                     icon="/assets/icons/appointments.svg" 
                 />
 
                 <StatCard
                     type="pending"
-                    count={5}
+                    count={appointments.pendingCount}
                     label="Pending Appointments"
                     icon="/assets/icons/pending.svg" 
                 />
 
                 <StatCard
                     type="cancelled"
-                    count={5}
+                    count={appointments.cancelledCount}
                     label="Cancelled Appointments"
                     icon="/assets/icons/cancelled.svg" 
                 />
             </section>
+
+            <DataTable columns={columns} data={appointments.documents} />
         </main>
     </div>
   )
--- lib/actions/appontment.actions.ts
+++ lib/actions/appontment.actions.ts
@@ -2,6 +2,7 @@
 import { ID, Query } from 'node-appwrite';
 import { APPOINTMENT_COLLECTION_ID, DATABASE_ID, databases } from '../appwrite.config';
 import { parseStringify } from '../utils';
+import { Appointment } from '@/types/appwrite.types';
 
 export const createAppointment  = async (appointment: CreateAppointmentParams) => {
     try {
@@ -40,8 +41,38 @@ export const getRecentAppointmentList = async () => {
             [Query.orderDesc(`$createdAt`)]    
         ) // descending order
 
-        
+        const initialCounts = {
+            scheduledCount: 0,
+            pendingCount: 0,
+            cancelledCount: 0
+        }
 
+
+        const counts = (appointments.documents as Appointment[]).reduce(
+            (acc, appointment) => {
+              switch (appointment.status) {
+                case "scheduled":
+                  acc.scheduledCount++;
+                  break;
+                case "pending":
+                  acc.pendingCount++;
+                  break;
+                case "cancelled":
+                  acc.cancelledCount++;
+                  break;
+              }
+              return acc;
+            },
+            initialCounts
+          );
+      
+          const data = {
+            totalCount: appointments.total,
+            ...counts,
+            documents: appointments.documents,
+          };
+
+          return parseStringify(data);
     } catch (error) {
         console.log(error)
     }
