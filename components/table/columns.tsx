16b23aca12192f7aee0902bbbf461a21a2de0729
Trishane <trishaneww@gmail.com>
2024-07-25 23:50:47 -0400

    maps over values

--- components/table/columns.tsx
+++ components/table/columns.tsx
@@ -4,18 +4,12 @@ import { ColumnDef } from "@tanstack/react-table"
 import { MoreHorizontal } from "lucide-react"
  
 import { Button } from "@/components/ui/button"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+
 import StatusBadge from "../StatusBadge"
 import { formatDateTime } from "@/lib/utils"
 import { Doctors } from "@/constants"
 import Image from "next/image"
+import AppointmentModal from "../AppointmentModal"
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
@@ -80,10 +74,22 @@ export const columns: ColumnDef<Payment>[] = [
   {
     id: "actions",
     header: () => <div className="pl-4">Actions</div>,
-    cell: ({ row }) => {
+    cell: ({ row: { original: data} }) => {
         return (
             <div className="flex gap-1">
-                AppointmentModal
+                <AppointmentModal
+                    type="schedule"
+                    patientId={data.patient.$id}
+                    userId={data.userId}
+                    appointmentId={data}
+                />
+                <AppointmentModal
+                    type="cancel"
+                    patientId={data.patient.$id}
+                    userId={data.userId}
+                    appointmentId={data}
+
+                />
             </div>
         )
     }
