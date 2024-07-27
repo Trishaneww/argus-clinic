9a125d93fd32d19c1745d8bee5b6415089fb66e1
Trishane <trishaneww@gmail.com>
2024-07-27 03:48:56 -0400

    checks if patient already exists

--- app/patients/[userid]/register/page.tsx
+++ app/patients/[userid]/register/page.tsx
@@ -7,7 +7,6 @@ import { getPatient, getUser } from "@/lib/actions/patient.actions";
 const Register = async ({ params: { userId } }: SearchParamProps) => {
   const user = await getUser(userId);
   const patient = await getPatient(userId);
-
   if (patient) redirect(`/patients/${userId}/new-appointment`);
 
   return (
